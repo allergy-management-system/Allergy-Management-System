bf1ad92998b5553b0478c56677f4b1fbad9c9c37
waglarry <emmanuelacquaah2468@gmail.com>
2024-03-05 15:59:36 +0000

    Update FormStepperView.java
    Update to show notification

--- frontend/src/main/java/com/example/application/views/component/formStepper/FormStepperView.java
+++ frontend/src/main/java/com/example/application/views/component/formStepper/FormStepperView.java
@@ -1,10 +1,12 @@
 package com.example.application.views.component.formStepper;
 
+import com.example.application.components.Notifications;
 import com.example.application.views.component.allergyForm.Form1;
 import com.example.application.views.component.allergyForm.Form2;
 import com.example.application.views.component.allergyForm.Form3;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.HtmlComponent;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
@@ -16,6 +18,7 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.dom.Style;
@@ -31,20 +34,6 @@ public class FormStepperView extends VerticalLayout {
     private Button submitButton;
     private int currentStep = 0;
     private Div[] formLayouts;
-
-    public static ComponentEventListener<ClickEvent<Button>> createClickHandler(
-            Supplier<Notification> notificationSupplier) {
-        return clickEvent -> {
-            Button btn = clickEvent.getSource();
-            btn.setEnabled(false);
-
-            Notification notification = notificationSupplier.get();
-            notification.setPosition(Notification.Position.MIDDLE);
-            notification.setDuration(5000);
-            notification.open();
-            notification.addDetachListener(detachEvent -> btn.setEnabled(true));
-        };
-    }
 
     public FormStepperView(FormFields formFields) {
         this.formFields = formFields;
@@ -139,7 +128,6 @@ public class FormStepperView extends VerticalLayout {
         dialog.addRejectListener(event -> Notification.show("Discarded"));
 
         dialog.setConfirmText("Save");
-//        dialog.addConfirmListener(event -> createClickHandler(FormStepperView::createFormSubmitSuccess));
         dialog.addConfirmListener(event -> SendDataToBackend());
         dialog.open();
 
@@ -151,47 +139,40 @@ public class FormStepperView extends VerticalLayout {
         System.out.println(formFields);
         System.out.println("*************");
 
-        Notification.show("Successfully Updated");
+        show().setClassName("notification-position");
+
     }
 
-    public static Notification createFormSubmitSuccess() {
+    public Notification show() {
+        // When creating a notification using the constructor,
+        // the duration is 0-sec by default which means that
+        // the notification does not close automatically.
         Notification notification = new Notification();
+        notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+        notification.setDuration(5000);
+        notification.setPosition(Notification.Position.TOP_END);
 
-        Icon icon = VaadinIcon.CHECK_CIRCLE.create();
-        icon.setColor("var(--lumo-success-color)");
+        Div text = new Div(
+                new Text("Form submission successful."),
+                new HtmlComponent("br"),
+                new Text("your allergy form has successfully  been submitted")
+        );
 
-        Div uploadSuccessful = new Div(new Text("Upload successful"));
-        uploadSuccessful.getStyle()
-                .set("font-weight", "600")
-                .setColor("var(--lumo-success-text-color)");
+        Button closeButton = new Button(new Icon("lumo", "cross"));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.setAriaLabel("Close");
+        closeButton.addClickListener(event -> {
+            notification.close();
+        });
 
-        Span fileName = new Span("Financials.xlsx");
-        fileName.getStyle()
-                .set("font-size", "var(--lumo-font-size-s)")
-                .set("font-weight", "600");
-
-        Div info = new Div(uploadSuccessful,
-                new Div(fileName, new Text(" is now available in "),
-                        new Anchor("#", "Documents")));
-
-        info.getStyle()
-                .set("font-size", "var(--lumo-font-size-s)")
-                .setColor("var(--lumo-secondary-text-color)");
-
-        var layout = new HorizontalLayout(icon, info,
-                createCloseBtn(notification));
+        HorizontalLayout layout = new HorizontalLayout(text, closeButton);
         layout.setAlignItems(Alignment.CENTER);
 
         notification.add(layout);
+        notification.open();
+
+        notification.setPosition(Notification.Position.MIDDLE);
 
         return notification;
-    }
-
-    public static Button createCloseBtn(Notification notification) {
-        Button closeBtn = new Button(VaadinIcon.CLOSE_SMALL.create(),
-                clickEvent -> notification.close());
-        closeBtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
-
-        return closeBtn;
     }
 }
