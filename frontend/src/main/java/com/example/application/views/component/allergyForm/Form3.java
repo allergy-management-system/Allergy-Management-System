da30326252b5a10350cfaa0dc426e250603b043f
waglarry <emmanuelacquaah2468@gmail.com>
2024-03-05 10:52:49 +0000

    Update Form3 input fields

--- frontend/src/main/java/com/example/application/views/component/allergyForm/Form3.java
+++ frontend/src/main/java/com/example/application/views/component/allergyForm/Form3.java
@@ -1,5 +1,6 @@
 package com.example.application.views.component.allergyForm;
 
+import com.example.application.views.component.formStepper.FormFields;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
@@ -12,6 +13,8 @@ import com.vaadin.flow.component.textfield.TextArea;
 @CssImport("./themes/frontend/AllergyForm.css")
 public class Form3 extends Div {
 
+    FormFields formFields = new FormFields();
+
     public Form3 () {
         H3 title = new H3("Food Allergy Form");
         Paragraph subTitle = new Paragraph("History and Current Status");
@@ -21,6 +24,8 @@ public class Form3 extends Div {
 
         Paragraph label1 = new Paragraph("Have you ever experienced any of these symptoms");
         TextArea experience = new TextArea();
+
+        formFields.setSymptomsExperienced(experience.getValue());
         experience.setWidthFull();
         experience.setHeight("120px");
         Div verticalGroup1 = new Div();
@@ -28,16 +33,20 @@ public class Form3 extends Div {
         verticalGroup1.add(label1, experience);
 
         Paragraph label2 = new Paragraph("Do you have any medication allergy?");
-        RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
-        radioGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
-        radioGroup.setItems("Yes", "No");
-        radioGroup.setValue("Yes");
+        RadioButtonGroup<String> medication = new RadioButtonGroup<>();
+        medication.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+        medication.setItems("Yes", "No");
+        medication.setValue("Yes");
+
+        formFields.setMedicationAllergy(medication.getValue());
         Div verticalGroup2 = new Div();
         verticalGroup2.addClassName("vertical-group");
-        verticalGroup2.add(label2, radioGroup);
+        verticalGroup2.add(label2, medication);
 
         Paragraph label3 = new Paragraph("Have you ever experienced any of these symptoms");
         TextArea notes = new TextArea();
+
+        formFields.setAdditionalNotes(notes.getValue());
         notes.setWidthFull();
         notes.setHeight("120px");
         Div verticalGroup3 = new Div();
