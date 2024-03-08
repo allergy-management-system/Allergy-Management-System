7ffa2abe77ad14592846507b5ad3d0ed5e8406e3
waglarry <emmanuelacquaah2468@gmail.com>
2024-03-08 16:19:21 +0000

    Update js files

--- frontend/target/dev-bundle/webapp/VAADIN/build/vaadin-virtual-list-lIVn3SLk-afdU6jIm.js
+++
@@ -1,1 +0,0 @@
-import{K as a}from"./indexhtml-_T3CYRT0.js";const i={tagName:"vaadin-virtual-list",displayName:"Virtual List",elements:[{selector:"vaadin-virtual-list",displayName:"List",properties:[a.backgroundColor,a.borderColor,a.borderWidth,a.borderRadius]}]};export{i as default};
